783b88872fdb2a78608d8de89587c440ee88b7fb
francoromeo <francoromeo5012@gmail.com>
2023-06-25 23:12:45 -0300

    js3

--- app.js
+++ app.js
@@ -11,9 +11,26 @@ credit_number.addEventListener('keyup', () => {
 
 const img = ["img/air1.png", "img/Airpods.png", "img/air3.png"];
 
+let dotoff = () => {
+    Array.from(document.getElementsByClassName('dot')).forEach((ele, i)=> {
+        ele.style.boxShadow = "unset";
+        ele.style.background = "#0070d6";
+    })
+}
+
+let cardoff = () => {
+Array.from(document.getElementsByClassName('card_img')).forEach((ele, i)=> {
+    ele.style.boxShadow = "unset";
+    })
+}
+
 Array.from(document.getElementsByClassName('card_img')).forEach((ele, i)=>{
     ele.addEventListener('click', () => {
+    cardoff();  
+    dotoff();
     document.getElementById('poster').src = img[i];
-    document.getElementsByClassName("card_img")[i].getElementsByClassName.boxShadow = "0px 0px 20px rgb(0,0,0,.2"
+    document.getElementsByClassName("card_img")[i].style.boxShadow = "0px 0px 20px rgb(0,0,0,.2";
+    document.getElementsByClassName('dot')[i].style.background = "transparent";
+    document.getElementsByClassName('dot')[i].style.boxShadow = "0px 0px 5px #0070d6";
     })
 })
